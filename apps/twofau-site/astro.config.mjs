8f696c600e8612909ad0d1404ee13d477baefd07
Nikolay Kostyurin <jilizart@gmail.com>
2026-08-26 16:40:18 +0200

    fix: lock file

--- apps/twofau-site/astro.config.mjs
+++ apps/twofau-site/astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
@@ -16,7 +14,5 @@ export default defineConfig({
       plugins: [
           tailwindcss(),
       ],
-  },
-
-  adapter: cloudflare()
+  }
 });
